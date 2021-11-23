c046d6b3373ecab5c6da2a39c9831ecb2ccb4e9f
201710757 <neti69@naver.com>
2021-11-24 01:55:53 +0900

    add remark

--- keras_model/predict.py
+++ keras_model/predict.py
@@ -5,19 +5,24 @@ import tensorflow as tf
 import numpy as np
 import sys
 
+# load dict
 with open('./data/word.pickle', 'rb') as f:
     dictionary_word = pickle.load(f)
 with open('./data/label.pickle', 'rb') as f:
     label_dict = pickle.load(f)
+
+# hardcoding
 max_len = 16
 
+# get sequence
+s = sys.argv[1]
 # s = "She has no bangs"
 # s = "He has long face"
 # s = "She has no beard, and has a square-shaped face"
 # s = "She is middle-aged white woman"
-s = sys.argv[1]
 
 
+# settings for model input
 test_input = np.empty((0, max_len), int)
 
 token = word_tokenize(s)
@@ -30,7 +35,10 @@ if len(tmp) != max_len:
     
 test_input = np.append(test_input, np.array([tmp]), axis=0)
 
+# load model
 model = tf.keras.models.load_model("./data/draw_model")
+
+# predict key
 print("predict : ", [k for k, v in label_dict.items() if v == np.argmax(model.predict(test_input))])
 
 
